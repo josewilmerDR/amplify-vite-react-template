0c320bd40975934fece49454afe82f046a4d7583
josewdr <50268140+josewdr@users.noreply.github.com>
2024-11-17 16:16:01 -0600

    add autentication

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-  const { signOut } = useAuthenticator();
+
+  const { user, signOut } = useAuthenticator();
+
+  // const { signOut } = useAuthenticator();
 
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
@@ -27,6 +30,7 @@ function App() {
 
   return (
     <main>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
